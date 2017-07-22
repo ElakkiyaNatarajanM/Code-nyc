a4e0636c9cd2ca109b640220f0ef908d34497958
ElakkiyaNatarajanM <30312838+ElakkiyaNatarajanM@users.noreply.github.com>
2017-07-22 11:22:45 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,20 @@
+    #include <stdio.h>
+    void main()
+    {
+      int i,fact=1,n;
+      printf("Enter the number \n");
+      scanf("%d", &n);
+      if (n<= 0)
+           fact = 1;
+           else
+           {
+             for (i=1;i<=n;i++)
+             {
+               fact=fact*i;
+               }
+                }
+                printf("Factorialof%d=%5d\n",n,fact);
+
+    }
+
+
